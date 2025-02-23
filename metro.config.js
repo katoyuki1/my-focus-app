9815785b6519475c0dac54d7feaa2b76b7a04668
yuuki.kato <0me3210g039530z@gmail.com>
2025-02-23 10:05:20 +0900

    temp backup

---
+++ metro.config.js
@@ -0,0 +1,14 @@
+const { getDefaultConfig } = require("expo/metro-config");
+
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
+
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-reanimated/plugin"),
+  };
+
+  return config;
+})();
