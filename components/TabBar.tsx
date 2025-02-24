6f0f6aa8c03421a28c801fb21d09c810bebf265d
yuuki.kato <0me3210g039530z@gmail.com>
2025-02-24 09:37:42 +0900

    ui change

--- components/TabBar.tsx
+++ components/TabBar.tsx
@@ -7,7 +7,7 @@ export const TabBar = () => {
       <View className="flex-row justify-around items-center h-full pb-4">
         <TouchableOpacity className="items-center">
           <Image
-            source={require("./assets/home-icon.png")}
+            source={require("../assets/home-icon.png")}
             className="w-6 h-6 mb-1"
           />
           <Text className="text-xs text-gray-600">Home</Text>
@@ -15,7 +15,7 @@ export const TabBar = () => {
 
         <TouchableOpacity className="items-center">
           <Image
-            source={require("./assets/quotes-icon.png")}
+            source={require("../assets/quotes-icon.png")}
             className="w-6 h-6 mb-1"
           />
           <Text className="text-xs text-gray-600">Quotes</Text>
@@ -24,7 +24,7 @@ export const TabBar = () => {
         <TouchableOpacity className="items-center">
           <View className="w-12 h-12 bg-blue-500 rounded-full justify-center items-center">
             <Image
-              source={require("./assets/settings-icon.png")}
+              source={require("../assets/settings-icon.png")}
               className="w-6 h-6"
               tintColor="white"
             />
