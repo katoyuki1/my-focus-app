e9ec7bbfa81535287e8dafccf46b4ed0e9b7dac5
yuuki.kato <0me3210g039530z@gmail.com>
2025-03-02 10:21:24 +0900

    change timer

--- components/TabBar.tsx
+++ components/TabBar.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export const TabBar = () => {
+  const navigation = useNavigation();
+
   return (
     <View className="h-20 bg-white border-t border-gray-200">
       <View className="flex-row justify-around items-center h-full pb-4">
-        <TouchableOpacity className="items-center">
+        {/* Home ボタン */}
+        <TouchableOpacity className="items-center" onPress={() => navigation.navigate("Home")}>
           <Image
             source={require("../assets/home-icon.png")}
             className="w-6 h-6 mb-1"
@@ -13,24 +17,52 @@ export const TabBar = () => {
           <Text className="text-xs text-gray-600">Home</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity className="items-center">
+        {/* Quotes ボタン */}
+        <TouchableOpacity className="items-center" onPress={() => navigation.navigate("Quotes")}>
           <Image
             source={require("../assets/quotes-icon.png")}
             className="w-6 h-6 mb-1"
           />
           <Text className="text-xs text-gray-600">Quotes</Text>
         </TouchableOpacity>
-
-        <TouchableOpacity className="items-center">
-          <View className="w-12 h-12 bg-blue-500 rounded-full justify-center items-center">
-            <Image
-              source={require("../assets/settings-icon.png")}
-              className="w-6 h-6"
-              tintColor="white"
-            />
-          </View>
-        </TouchableOpacity>
       </View>
     </View>
   );
 };
+
+// import React from "react";
+// import { View, Text, TouchableOpacity, Image } from "react-native";
+
+// export const TabBar = () => {
+//   return (
+//     <View className="h-20 bg-white border-t border-gray-200">
+//       <View className="flex-row justify-around items-center h-full pb-4">
+//         <TouchableOpacity className="items-center">
+//           <Image
+//             source={require("../assets/home-icon.png")}
+//             className="w-6 h-6 mb-1"
+//           />
+//           <Text className="text-xs text-gray-600">Home</Text>
+//         </TouchableOpacity>
+
+//         <TouchableOpacity className="items-center">
+//           <Image
+//             source={require("../assets/quotes-icon.png")}
+//             className="w-6 h-6 mb-1"
+//           />
+//           <Text className="text-xs text-gray-600">Quotes</Text>
+//         </TouchableOpacity>
+
+//         {/* <TouchableOpacity className="items-center">
+//           <View className="w-12 h-12 bg-blue-500 rounded-full justify-center items-center">
+//             <Image
+//               source={require("../assets/settings-icon.png")}
+//               className="w-6 h-6"
+//               tintColor="white"
+//             />
+//           </View>
+//         </TouchableOpacity> */}
+//       </View>
+//     </View>
+//   );
+// };
